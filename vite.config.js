6c8b881011bc1524d3f6a8cd5d33ab3b569b1bd3
ethicalgreyhatCoder <dj001.mandal@gmail.com>
2025-12-14 01:03:44 +0530

    Allow natlify app to host

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'devserver-main--dreamy-kleicha-1c5f18.netlify.app'
+    ]
+  },
   base: "/KalpanaPortfolio/",
 })
