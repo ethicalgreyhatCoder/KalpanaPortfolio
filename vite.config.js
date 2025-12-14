331fe7472653e86a5475a56439a47082fbd66897
ethicalgreyhatCoder <dj001.mandal@gmail.com>
2025-12-15 01:58:03 +0530

    Full working project version But refactoring will happen

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     allowedHosts: [
       'devserver-main--dreamy-kleicha-1c5f18.netlify.app'
